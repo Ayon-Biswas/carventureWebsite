79219b22e43916eedc25f9b563d701ad074de975
Ayon Biswas <ayonshirsho@gmail.com>
2024-02-09 02:33:14 +0600

    user management and search added

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -19,13 +19,16 @@
                             <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link text-white" href="users.php">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="user_queries.php">User Queries</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link text-white" href="cars.php">Cars</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="feature_facilities.php">Features & Facilities</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="user_queries.php">User Queries</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="carousel.php">Carousel</a>
