8901a460a92d2e85308ed322a6f1beb62cfd43cb
Ayon Biswas <ayonshirsho@gmail.com>
2024-03-01 21:16:18 +0600

    final touches

--- index.php
+++ index.php
@@ -7,20 +7,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <?php require ('inc/links.php') ?>
   <title><?php echo $settings_r['site_title']?> - HOME</title> 
-  <style>
-    .availability-form {
-      margin-top: -50px;
-      z-index: 2;
-      position: relative;
-    }
 
-    @media screen and (max-width: 575px) {
-      .availability-form {
-        margin-top: 25px;
-        padding: 0 35px;
-      }
-    }
-  </style>
 </head>
 
 <body class="bg-light">
@@ -48,45 +35,7 @@
     </div>
   </div>
 
-  <!-- check availability form -->
-  <!-- <div class="container availability-form">
-    <div class="row">
-      <div class="col-lg-12 bg-white shadow p-4 rounded">
-        <h5 class="mb-4">Check Booking Availability</h5>
-        <form>
-          <div class="row align-items-end">
-            <div class="col-lg-3 mb-3">
-              <label class="form-label" style="font-weight:500;">Pick-up</label>
-              <input type="date" class="form-control shadow-none">
-            </div>
-            <div class="col-lg-3 mb-3">
-              <label class="form-label" style="font-weight:500;">Drop-off</label>
-              <input type="date" class="form-control shadow-none">
-            </div>
-            <div class="col-lg-3 mb-3">
-              <label class="form-label" style="font-weight:500;">Adult</label>
-              <select class="form-select shadow-none">
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="col-lg-2 mb-3">
-              <label class="form-label" style="font-weight:500;">Children</label>
-              <select class="form-select shadow-none">
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="col-lg-1 mb-lg-3 mt-2">
-              <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div> -->
+
 
   <!--Our Cars -->
   <h2 class="mt-5 pt-4 text-center fw-bold h-font">OUR CARS</h2>
